c8dfa79a5c35de562c5159b33e6509c72ab6f468
ChangshengZhai <2926225800@qq.com>
2020-03-09 23:47:13 +0800

    梯度练习

---
+++ p43dataset_load.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from pandas import DataFrame
+from sklearn import datasets
+
+x_data = datasets.load_iris().data  #.data返回iris数据集所有输入特征
+y_data = datasets.load_iris().target  #.target返回iris数据集所有标签
+
+print("x_data from datasets: \n",x_data)
+print("y_data from datasets: \n",y_data)
+
+x_data = DataFrame(x_data, columns=['花萼长度','花萼宽度','花瓣长度','花瓣宽度'])  #加列名
+pd.set_option('display.unicode.east_asian_width',True)  # 设置列名对齐
+print("x_data add index: \n",x_data)
+
+x_data['类别'] = y_data  # 新加一列，列标签为“类别”，数据为y_data
+print("x_data add a column：\n", x_data)
